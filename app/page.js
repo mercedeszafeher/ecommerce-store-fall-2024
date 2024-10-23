1839c950c82be6bf9f04503fd6012545bc32ba4c
mercedeszafeher <mercedesz.a.feher@gmail.com>
2024-10-23 22:44:06 +0200

    adding hi

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
+import BeerShowcase from './home/BeerShowCase';
 import styles from './page.module.scss';
-import ProductPage from './products/page';
 
 export default function HomePage() {
-  return (
-    <div className={styles.homePage}>
-      <h1>WanderBrew</h1>
-      <ProductPage />
-    </div>
-  );
+  return <div className={styles.homePage}></div>;
 }
